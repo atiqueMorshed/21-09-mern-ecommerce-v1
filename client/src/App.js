2d3bea906569a252290a3bc4ceb1e0e6b5a65506
Atique Morshed Sami <iamsamix17@gmail.com>
2021-09-23 08:01:56 +0600

    +sass, +react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import Header from "./components/header/header.component";
+import Footer from "./components/footer/footer.component";
+
+import Container from "react-bootstrap/Container";
+
+import "./App.scss";
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to MERN Ecomm v1</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to MERN Ecomm v1</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
